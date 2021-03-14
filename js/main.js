eba1494e74457bd9fc30e0b544c288d30da19c50
milleseseau <leojimlavi@gmail.com>
2021-03-14 01:44:50 -0500

    fixing main.js

--- js/main.js
+++ js/main.js
@@ -10,7 +10,50 @@ const scoreboard = {
 
 // Play game
 function play(e) {
-    console.log(e.target.id);
+    restart.style.display = 'inline-block';
+    const playerChoice = e.target.id;
+    const computerChoice = getComputerChoice();
+    const winner = getWinner(playerChoice, computerChoice);
+
+    console.log(playerChoice, computerChoice, winner);
+}
+
+
+//Get computers choice
+function getComputerChoice() {
+    const rand = Math.random();
+    if(rand < 0.34) {
+        return 'rock'; 
+    }   else if(rand <= 0.67) {
+        return 'paper';  
+    }   else {
+        return 'scissors';
+    }
+}
+
+// Get game winner
+function getWinner(p, c) {
+    if (p === c) {
+        return 'draw';
+    } else if (p === 'rock') {
+        if (c === 'paper') {
+            return 'computer';
+        } else {
+            return 'player';
+        }
+    } else if(p === 'paper') {
+        if (c === 'scissors') {
+            return 'computer';
+        } else {
+            return 'player'
+        }
+    } else if (p === 'scissors') {
+        if (c === 'rock') {
+            return 'computer';
+        } else {
+            return 'player';
+        }
+    }
 }
 
 // Event listeners
